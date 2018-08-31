2710d2b04e8632fb6fc403387164e6a74177e55c
dumblole <move2234@gmail.com>
2018-08-31 14:18:46 -0400

    Added public redirecting. ex. When user tries to return to login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,11 @@ import { startLogout } from "../actions/auth";
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
+    <NavLink to="/dashboard" activeClassName="is-active">
       Go Home
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
       Go Create
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Go Help
     </NavLink>
     <button onClick={startLogout}>Logout</button>
   </header>
--- src/components/HelpPage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const HelpPage = () => <div>Help Page</div>;
-
-export default HelpPage;
